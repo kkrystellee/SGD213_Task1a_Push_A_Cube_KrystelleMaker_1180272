13c14d84504370c77d4ae0b7ccae46afa718cdd6
kkrystellee <k_m276@student.usc.edu.au>
2026-03-11 15:28:45 +1000

    Adjusted Scripts
    Deleted the movement script as it had no purpose. Removed the function DoSomething in PlayerController as it had no purpose. Corrected player controller script with correct indentation, camelcase, naming and comments. The script was also changed to say You Won The Game instead of You Win

--- SGD213 Task1/Assets/RollABall/Scripts/PlayerController.cs
+++ SGD213 Task1/Assets/RollABall/Scripts/PlayerController.cs
@@ -1,53 +1,71 @@
-﻿using UnityEngine;
-
-// Include the namespace required to use Unity UI
+﻿// Include the namespace required to use Unity UI
+using UnityEngine;
 using UnityEngine.UI;
-
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
     // Create public variables for player speed, and for the Text UI game objects
     [SerializeField]
-    public float f_horPlayAccel;
+    public float playerAcceleration;
+	public Text numTotal;
+	public Text winText;
 
-	public Text NumTotal;
-	public Text wintext;
-
-	// Create private references to the rigidbody component on the player, and the count of pick up objects picked up so far
+	// Create private references to the player's rigidbody component and the current pick up count
 	private Rigidbody rb;
-	private float inttotalcount;
+	private int inTotalCount;
 
-	// At the start of the game..
-	void Start ()	{rb = GetComponent<Rigidbody>();inttotalcount = 0;SetCountText ();wintext.text = "";}
+	// Runs once the game starts 
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
 
+		// At the start of the game, the count is set to 0
+		inTotalCount = 0;
+
+		// Update UI text
+		SetCountText ();
+		winText.text = "";
+	}
+
+	// Runs every physics update
 	void FixedUpdate ()
 	{
 		// Set some local float variables equal to the value of our Horizontal and Vertical Inputs
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
+
 		// Create a Vector3 variable, and assign X and Z to feature our horizontal and vertical float variables above
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		rb.AddForce (movement * f_horPlayAccel);
+		rb.AddForce (movement * playerAcceleration);
 	}
-	void OnTriggerEnter(Collider other) 
-{
-if (other.gameObject.CompareTag ("Pick Up"))
-{							other.gameObject.SetActive (false);
-							inttotalcount = inttotalcount + 1;
-							SetCountText ();
-		}}
 
-    void DoSomething()
+	// Runs when the player collides with a trigger object
+	void OnTriggerEnter(Collider other) 
 	{
-	
+		// Checks if object is tagged "Pick Up"
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			// Hide the Pick Up
+			other.gameObject.SetActive (false);
+
+			// Increase the count by 1
+			inTotalCount++;
+
+			// Update UI with new count
+			SetCountText ();
+		}
 	}
-    void SetCountText()
-{NumTotal.text = "Count: " + inttotalcount.ToString ();
-		if (inttotalcount >= 12) 
-{
-			// Set the text value of our 'winText'
-			wintext.text = "You Win!";
-}
-}
-}
+
+	// Updates the pickup counter value
+	void SetCountText()
+	{
+		numTotal.text = "Count: " + inTotalCount.ToString ();
+		
+		// Check if player has collected enough pickups to win
+		if (inTotalCount >= 12) 
+		{
+			winText.text = "You Won the game!!";
+		}
+	}
+}	
